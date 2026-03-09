7d1383b55b9a5db1b3f23258926d660600e85559
Ralha-igor <ralhaigor@gmail.com>
2026-03-09 09:08:24 -0300

    firebase

---
+++ app/src/main/java/br/com/igor/microredesocial/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.igor.microredesocial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.com.igor.microredesocial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
