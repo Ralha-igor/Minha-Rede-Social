349b00ec3cb03157bebd94ff6e173deb61ac1710
Ralha-igor <ralhaigor@gmail.com>
2026-03-09 11:27:02 -0300

    tela de login-cadastro

--- app/src/main/java/br/com/igor/microredesocial/MainActivity.kt
+++ app/src/main/java/br/com/igor/microredesocial/MainActivity.kt
@@ -1,19 +1,84 @@
 package br.com.igor.microredesocial
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.com.igor.microredesocial.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupFirebase()
+        setupListeners()
+    }
+
+    private fun setupFirebase() {
+        firebaseAuth = FirebaseAuth.getInstance()
+    }
+
+    private fun setupListeners() {
+
+        binding.btnLogin.setOnClickListener {
+            autenticarUsuario()
+        }
+
+        binding.btnGoToSignUp.setOnClickListener {
+
+            startActivity(
+                Intent(this, SignUpActivity::class.java)
+            )
+
+        }
+    }
+
+    private fun autenticarUsuario() {
+
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+
+            Toast.makeText(
+                this,
+                "Preencha todos os campos",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            return
+        }
+
+        firebaseAuth
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+
+                    startActivity(
+                        Intent(this, HomeActivity::class.java)
+                    )
+
+                    finish()
+
+                } else {
+
+                    Toast.makeText(
+                        this,
+                        "Erro no login",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+
+            }
     }
 }
---
+++ app/src/main/java/br/com/igor/microredesocial/SignUpActivity.kt
@@ -0,0 +1,63 @@
+package br.com.igor.microredesocial
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.com.igor.microredesocial.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnCreateAccount.setOnClickListener {
+
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+            val confirmPassword = binding.edtConfirmPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if (password != confirmPassword) {
+
+                Toast.makeText(this, "As senhas não coincidem", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            firebaseAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+
+                    if (task.isSuccessful) {
+
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+
+                    } else {
+
+                        Toast.makeText(
+                            this,
+                            task.exception?.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                    }
+
+                }
+        }
+    }
+}
